7e5cfdbad1f35afc786aac3f88176f39f3ebc206
Andy Choi <wooseokchoi96@gmail.com>
2019-10-10 15:57:21 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,47 +1,45 @@
 import React from 'react'
 
-class Pet extends React.Component {
+const Pet = (props) => {
   
-  showGender = () => {
-    if (this.props.pet.gender === 'male'){
+  let showGender = () => {
+    if (props.pet.gender === 'male'){
       return '♂'
-    } else if (this.props.pet.gender === 'female'){
+    } else if (props.pet.gender === 'female'){
       return '♀'
     }
   }
 
-  showAdoptionStatus = () => {
-    // console.log(this.props.pet)
-    if (this.props.pet.isAdopted){
+  let showAdoptionStatus = () => {
+    if (props.pet.isAdopted){
       return (<button className="ui disabled button">Already adopted</button>)
     } else {
-      return (<button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>)
+      return (<button className="ui primary button" onClick={() => props.onAdoptPet(props.pet.id)}>Adopt pet</button>)
     }
   }
   
-  render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {this.props.pet.name}
-            &nbsp;&nbsp;
-            {this.showGender()}
-          </a>
-          <div className="meta">
-            <span className="date">{this.props.pet.type}</span>
-          </div>
-          <div className="description">
-            <p>Age: {this.props.pet.age}</p>
-            <p>Weight: {this.props.pet.weight}</p>
-          </div>
+  return (
+    <div className="card">
+      <div className="content">
+        <a className="header">
+          {props.pet.name}
+          &nbsp;&nbsp;
+          {showGender()}
+        </a>
+        <div className="meta">
+          <span className="date">{props.pet.type}</span>
         </div>
-        <div className="extra content">
-          {this.showAdoptionStatus()}
+        <div className="description">
+          <p>Age: {props.pet.age}</p>
+          <p>Weight: {props.pet.weight}</p>
         </div>
       </div>
-    )
-  }
+      <div className="extra content">
+        {showAdoptionStatus()}
+      </div>
+    </div>
+  )
 }
+
 
 export default Pet
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 import Pet from './Pet'
 
-class PetBrowser extends React.Component {
-
-  render() {
-    let mapPets = this.props.pets.map(pet => <Pet onAdoptPet={this.props.onAdoptPet} key={pet.id} pet={pet}/>) ;
-    return <div className="ui cards">{mapPets}</div>
-  }
+const PetBrowser = (props) => {
+  let mapPets = props.pets.map(pet => <Pet onAdoptPet={props.onAdoptPet} key={pet.id} pet={pet}/>) ;
+  return <div className="ui cards">{mapPets}</div>
 }
 
 export default PetBrowser
